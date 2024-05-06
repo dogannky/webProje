5b71c02bc6b176c19ce42cc8429abdb109b8beb7
doğan kaya <dogankaya2555@gmail.com>
2024-05-06 17:11:35 +0300

    log in ,iletisim,main sayfaları revize edildi.

--- text.php
+++ text.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $parola = $_POST["parola"];
 
     // Kullanıcı adı ve parolayı doğrula
-    if ($kullaniciAdi == "skillup" && $parola == "skillup") {
+    if ($kullaniciAdi == "dodo" && $parola == "1234") {
         echo "Hoş geldiniz!";
     } else {
         echo "Lütfen doğru kullanıcı adı ve parolayı girin.";
